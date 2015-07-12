340affa189c6407090fe5b77f87b36242b1f6652
Brian Foshee <brianfoshee@gmail.com>
2015-07-12 10:56:42 -0400

    cases are in another file

--- go/leap/leap_test.go
+++ go/leap/leap_test.go
@@ -4,17 +4,6 @@ import (
 	"testing"
 )
 
-var testCases = []struct {
-	year        int
-	expected    bool
-	description string
-}{
-	{1996, true, "a vanilla leap year"},
-	{1997, false, "a normal year"},
-	{1900, false, "a century"},
-	{2400, true, "an exceptional century"},
-}
-
 func TestLeapYears(t *testing.T) {
 	for _, test := range testCases {
 		observed := IsLeapYear(test.year)
